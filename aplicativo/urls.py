03e11b175ea667b87421f8e72901c1cfe52a96a4
Talita Cristina Alves Lobato <05058225204@>
2025-04-10 11:31:26 -0300

    ..

--- aplicativo/urls.py
+++ aplicativo/urls.py
@@ -4,4 +4,6 @@ from django.urls import path
 urlpatterns = [
     path('lista/', view=lista_personagens, name='lista_personagens'),
     path('criar/', view=criar_bruxo, name='criar_bruxo'),
+    path('atualizar/<int:pk>', view=atualizar_bruxo, name='atualizar_bruxo'),
+
 ]
